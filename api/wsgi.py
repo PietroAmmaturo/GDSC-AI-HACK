088e627159350a3e60e8b0e7e400d795af5cf572
pietro <pammaturo02@gmail.com>
2024-04-20 13:47:04 +0200

    Auth

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for api project.
 
-It exposes the WSGI callable as a module-level variable named ``app``.
+It exposes the WSGI callable as a module-level variable named ``api``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
